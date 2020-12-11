65383a7ceceaec4cd58b6251ef00790573a05665
Kavya Sahithi <kavya.lily@gmail.com>
2020-12-11 18:02:44 +0100

    temp fix
    Signed-off-by: Kavya Sahithi <kavya.lily@gmail.com>

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import './About.css'
-import AboutItem from './AboutItem'
 
 function About() {
   return (
     <div className='acards'>
       <h1>Skills</h1>
-      <div className='acards__container'>
-        <div className='acards__wrapper'>
-          <ul className='acards__items'>
-            <AboutItem src='images/img-1.jpg' text='HTML 5' />
-            <AboutItem src='images/img-2.jpg' text='CSS 3' />
-            <AboutItem src='images/img-3.jpg' text='JS ES6' />
-            <AboutItem src='images/img-1.jpg' text='React' />
-            <AboutItem src='images/img-2.jpg' text='Bootstrap' />
-            <AboutItem src='images/img-3.jpg' text='Sass' />
-          </ul>
-        </div>
+      <div className='skills'>
+        <img src='images/IMG-2499.PNG' alt='yopa' className='img' />
+        <img src='images/IMG-2500.PNG' alt='yopa' className='img' />
+        <img src='images/IMG-2501.PNG' alt='yopa' className='img' />
+        <img src='images/IMG-2502.PNG' alt='yopa' className='img' />
       </div>
     </div>
   )
