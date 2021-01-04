16ab89b13281507bc0912dcf28704cda2297b76b
Kavya Sahithi <kavya.lily@gmail.com>
2021-01-04 12:37:07 +0100

    updated newer projects

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -25,39 +25,39 @@ function Cards() {
           <div className='cards__wrapper'>
             <ul className='cards__items'>
               <CardItem
-                src='images/Github finder.png'
-                text='Search github users and display their profile info & repos'
-                label='React Hooks, router, context, axios & custom CSS '
-                live='https://github-finder-kavya.netlify.app/'
-                code='https://github.com/KavyaSahithi/Github-Finder'
-              ></CardItem>
-              <CardItem
                 src='images/Green Seat.png'
-                text='E-commerce site for buying furniture from different brands'
-                label='React context,Hooks,axios'
+                text='E-commerce Site For Buying Furniture From Different Brands'
+                label='React context, Hooks, Axios'
                 live='https://green-seat-co.netlify.app/'
                 code='https://github.com/KavyaSahithi/Green-Seat.-Co'
               />
+              <CardItem
+                src='images/Github finder.png'
+                text='Search Github Users, Display Their Profile Info & Repos'
+                label='React Hooks, Router, Context, Axios & Custom CSS '
+                live='https://github-finder-kavya.netlify.app/'
+                code='https://github.com/KavyaSahithi/Github-Finder'
+              ></CardItem>
             </ul>
 
             <ul className='cards__items'>
               <CardItem
                 src='images/Budget app.png'
-                text='Keep track of income-expenditure ratio & calculate net savings'
+                text='Keep Track Of Income-Expenditure Ratio & Calculate Net Savings'
                 label='Vanilla JS, DOM manipulation, CSS'
                 live='https://kavyasahithi.github.io/Budget-App/'
                 code='https://github.com/KavyaSahithi/Budget-App'
               />
               <CardItem
                 src='images/malinis kitchen.png'
-                text='Website for a home-kitchen service in Bangalore, India'
-                label='HTML,CSS & JS'
+                text='Website For A Home-Kitchen Service In Bangalore, India'
+                label='HTML, CSS, JavaScript'
                 live='https://mailinis-kitchen.netlify.app/'
                 code='https://github.com/KavyaSahithi/Malini-s-Kitchen'
               />
               <CardItem
                 src='images/dice game.png'
-                text='2 player dice game (with beginner and pro versions)'
+                text='2 Player Dice Game (With Beginner & Pro Levels)'
                 label='Vannila JS, CSS'
                 live='https://kavyasahithi.github.io/Dice-Game'
                 code='https://github.com/KavyaSahithi/Dice-Game'
@@ -67,24 +67,47 @@ function Cards() {
             <ul className='cards__items'>
               <CardItem
                 src='images/meme generator.png'
-                text='Meme generator'
-                label='React & API call'
+                text='Meme Generator'
+                label='React, API call'
                 live='https://meme-generator-kavya.netlify.app/'
                 code='https://github.com/KavyaSahithi/Meme-generator'
               />
               <CardItem
-                src='images/bakes.png'
-                text='Bakery website'
-                label='HTML, CSS & flexbox'
-                live='https://kavyasahithi.github.io/Rosea-bakes/'
-                code='https://github.com/KavyaSahithi/Rosea-bakes'
+                src='images/weather app.png'
+                text='Weather Monitor App'
+                label='JavaScript, API call'
+                live='https://weather-monitor-app.netlify.app'
+                code='https://github.com/KavyaSahithi/Weather-monitor'
               />
+              <CardItem
+                src='images/bootstrap site.png'
+                text='Gifting Service Website With Shipping Form'
+                label='HTML, CSS, Bootstrap'
+                live='https://unicorn-favours.netlify.app'
+                code='https://github.com/KavyaSahithi/Bootstrap-gifting-website'
+              />
+            </ul>
+            <ul className='cards__items'>
               <CardItem
                 src='images/Todo list.png'
                 text='Simple Task List'
                 label='React'
                 live='https://task-list-1.netlify.app/'
                 code='https://github.com/KavyaSahithi/Todo-list'
+              />
+              <CardItem
+                src='images/animation.png'
+                text='Animation landing page'
+                label='CSS, Gsap'
+                live='https://landing-page-animation.netlify.app/'
+                code='https://github.com/KavyaSahithi/Landing-page-animation'
+              />
+              <CardItem
+                src='images/bakes.png'
+                text='Bakery Website'
+                label='HTML, CSS, Flexbox'
+                live='https://kavyasahithi.github.io/Rosea-bakes/'
+                code='https://github.com/KavyaSahithi/Rosea-bakes'
               />
             </ul>
           </div>
